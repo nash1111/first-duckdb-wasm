17a9b44ce4def9db0d4c12462dd47f51bf202a65
nash1111 <nash1111@users.noreply.github.com>
2024-11-27 21:59:03 +0900

    refactor: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path"
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -10,4 +10,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
