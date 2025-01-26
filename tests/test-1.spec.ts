5e2ce53bdec980b8c5ab6839c3385f35d26c4333
nash1111 <nash1111@users.noreply.github.com>
2025-01-26 19:59:38 +0900

    feat: chromium test passed

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,6 +3,9 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
+  // wait 3 sec
+  // TODO: wait for wasm module loading
+  await page.waitForTimeout(3000);
   const [fileChooser] = await Promise.all([
     page.waitForEvent('filechooser'),
     page.getByText('Upload CSV(Choose file or').click()
@@ -11,7 +14,7 @@ test('test', async ({ page }) => {
   await page.getByRole('button', { name: 'Confirm table name, column' }).click();
   await page.locator('.view-lines').click();
   await page.getByRole('textbox', { name: 'Editor content' }).fill('select * from bar_chart_sample;');
-  await page.getByRole('button').filter({ hasText: 'Run Query (CTRL+ENTER)' }).click();
+  await page.getByRole('button', { name: 'Run Query (CTRL+ENTER)' }).click();
   await page.getByRole('combobox').filter({ hasText: 'Bar Chart' }).click();
   await page.getByRole('option', { name: 'Area Chart' }).click();
 });
